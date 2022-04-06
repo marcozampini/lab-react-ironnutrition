6bababc24ca8fe4737fc10b8dbe6d9900c62c6ea
Marco Zampini <marco@bloom-it.io>
2022-04-06 15:26:07 +0200

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -8,17 +8,12 @@ function App() {
 
   return (
     <div className="App">
-      {foodsList.map((foodItem) => (
-        <FoodBox food={foodItem} key={foodItem.name} />
-      ))}
-      <FoodBox
-        food={{
-          name: 'Orange',
-          calories: 85,
-          image: 'https://i.imgur.com/abKGOcv.jpg',
-          servings: 1,
-        }}
-      />
+      <h1>Food List</h1>
+      <div className="food-list">
+        {foodsList.map((foodItem) => (
+          <FoodBox food={foodItem} key={foodItem.name} />
+        ))}
+      </div>
     </div>
   );
 }
